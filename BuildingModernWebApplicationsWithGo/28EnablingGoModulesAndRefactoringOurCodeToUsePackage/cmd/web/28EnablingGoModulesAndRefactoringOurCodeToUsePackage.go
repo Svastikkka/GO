2b7836e8285d3a28e6500d0887ee857779f4112a
svastikkka <ManshuSharma143Repo@gmail.com>
2023-03-25 18:48:55 +0530

    refactor: set correct mod uri

--- BuildingModernWebApplicationsWithGo/28EnablingGoModulesAndRefactoringOurCodeToUsePackage/cmd/web/28EnablingGoModulesAndRefactoringOurCodeToUsePackage.go
+++ BuildingModernWebApplicationsWithGo/28EnablingGoModulesAndRefactoringOurCodeToUsePackage/cmd/web/28EnablingGoModulesAndRefactoringOurCodeToUsePackage.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/svastikkka/GO/BuildingModernWebApplicationsWithGo/pkg/handlers"
+	"github.com/svastikkka/GO/BuildingModernWebApplicationsWithGo/28EnablingGoModulesAndRefactoringOurCodeToUsePackage/pkg/handlers"
 	"net/http"
 )
 
