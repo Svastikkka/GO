38bde5068905f0c955c5808d3ff17be50e4225ef
Manshu Sharma <manshu.sharma@digite.com>
2023-04-01 11:44:37 +0530

    add: common configuration setup

--- BuildingModernWebApplicationsWithGo/33SettingApplicationWideConfiguration/cmd/web/33SettingApplicationWideConfiguration.go
+++ BuildingModernWebApplicationsWithGo/33SettingApplicationWideConfiguration/cmd/web/33SettingApplicationWideConfiguration.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 
 	"github.com/svastikkka/GO/BuildingModernWebApplicationsWithGo/33SettingApplicationWideConfiguration/pkg/handlers"
+	"github.com/svastikkka/GO/BuildingModernWebApplicationsWithGo/33SettingApplicationWideConfiguration/pkg/render"
+	"github.com/svastikkka/GO/BuildingModernWebApplicationsWithGo/33SettingApplicationWideConfiguration/pkg/config"
 )
 
 const port = ":8080"
 
 func main() {
-	fmt.Print("hello world")
+	fmt.Print("Starting server at port %d", port)
+	var app config.AppConfig
+	tc, err := render.createTemplateCache()
+	if err != nil{
+		log.Fatal("Cannot create template cache")
+	}
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 	_ = http.ListenAndServe(port, nil)
