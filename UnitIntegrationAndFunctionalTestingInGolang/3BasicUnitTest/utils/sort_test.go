68092ba3aed3fbaa71f093261e956cdc45fba5e2
Manshu Sharma <manshu.sharma@digite.com>
2023-04-21 13:55:04 +0530

    add: validation for stage for BubbleSort

--- UnitIntegrationAndFunctionalTestingInGolang/3BasicUnitTest/utils/sort_test.go
+++ UnitIntegrationAndFunctionalTestingInGolang/3BasicUnitTest/utils/sort_test.go
@@ -1,8 +1,16 @@
 package sort
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBubbleSort(t *testing.T) {
 	elements := []int{9, 6, 4, 3, 2, 1, 3, 4}
 	BubbleSort(elements)
+	if elements[0] != 1 {
+		t.Error("First element should be 9")
+	}
+	if elements[7] != 9 {
+		t.Error("First element should be 1")
+	}
 }
